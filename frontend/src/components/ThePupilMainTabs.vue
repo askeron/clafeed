35e2036a13dfb88943828dae1fe909a156b5931d
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-05-30 03:17:08 +0200

    bootstrap

--- frontend/src/components/ThePupilMainTabs.vue
+++ frontend/src/components/ThePupilMainTabs.vue
@@ -12,7 +12,17 @@ const props = defineProps({
 
 <template>
   <header class="d-flex justify-content-center py-3">
-    <ul class="nav nav-pills">
+    <ul class="nav nav-pills nav-fill">
+    <li class="nav-item">
+      <RouterLink to="/" :class="{ 'nav-link': true, active: activeIndex === 0 }">Meine Räume</RouterLink>
+    </li>
+    <li class="nav-item">
+      <RouterLink to="/join" :class="{ 'nav-link': true, active: activeIndex === 1 }">Raum beitreten</RouterLink>
+    </li>
+    </ul>
+  </header>
+  <header class="justify-content-center py-3">
+    <ul class="nav nav-pills nav-fill">
     <li class="nav-item">
       <RouterLink to="/" :class="{ 'nav-link': true, active: activeIndex === 0 }">Meine Räume</RouterLink>
     </li>
