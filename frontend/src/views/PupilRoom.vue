74f16402a9ee178106ff1f153e09aeb7466df5ea
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-06-01 05:48:15 +0200

    added color to correct answer indicators

--- frontend/src/views/PupilRoom.vue
+++ frontend/src/views/PupilRoom.vue
@@ -118,11 +118,11 @@ onMounted(() => {
               <div class="col-1">
               </div>
               <div class="col-1 px-1" v-if="modeData.closeAndShowResults">
-                <svg v-if="modeData.correctAnswerIndex === index" xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="white" class="bi bi-check-circle" viewBox="0 0 16 16">
+                <svg v-if="modeData.correctAnswerIndex === index" xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="white" class="bi bi-check-circle svg-green" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                   <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
                 </svg>
-                <svg v-else xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="white" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <svg v-else xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="white" class="bi bi-x-circle svg-red" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
@@ -355,5 +355,12 @@ onMounted(() => {
   </div>
 </template>
 
-<style>
+<style scoped>
+.svg-red path {
+    fill: red;
+}
+.svg-green path {
+    fill: green;
+}
+
 </style>
